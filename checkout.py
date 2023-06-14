c0ca74d467aa217f02036195f5feb5009d017232
Sugita <107387702+sugitaaryandana1@users.noreply.github.com>
2023-06-14 08:59:40 +0800

    Update checkout.py

--- checkout.py
+++ checkout.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
 	options.add_argument('--log-level=3')
 
 	# Provide the path of chromedriver present on your system.
-	driver = webdriver.Chrome(executable_path="chromedriver",
-                              chrome_options=options)
+	driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
 	# driver = webdriver.Chrome(ChromeDriverManager().install())
 	# driver = webdriver.Chrome()
 	driver.set_window_size(480,360)
